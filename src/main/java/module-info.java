0deb6d0ef399c4bee93a7ca9fa302748c9cdf1f7
Gabriel Pérez <gabrielperezlago.dev@gmail.com>
2025-10-06 16:33:07 +0200

    CommitClase 25.10.06 Primer Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.gabriel.apalabrados.apalabrados {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.gabriel.apalabrados.apalabrados to javafx.fxml;
+    exports com.gabriel.apalabrados.apalabrados;
+}
